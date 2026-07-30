54d7d4e37bc3af5978c7576f74db7053b1e00931
Efraim <tprhoahau156348@email.com>
2026-07-30 16:34:41 +0300

    File update

--- src/app.js
+++ src/app.js
@@ -2,19 +2,20 @@ import express from 'express'
 import soldierController from './controllers/soldierController.js'
 import budgetController from './controllers/budgetController.js'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.post('/soldiers/:soldierId/benefits', soldierController.postSolder);
-app.get('/soldiers/:soldierId/benefits', soldierController.getSolder);
-app.patch('/soldiers/:soldierId/benefits', soldierController.patchSolder);
+app.post('/soldiers/:soldierId/benefits', soldierController.postSolder)
+app.get('/soldiers/:soldierId/benefits', soldierController.getSolder)
+app.patch('/soldiers/:soldierId/benefits', soldierController.patchSolder)
 
-app.post('/budget', budgetController.postBudget);
-app.get('/budget', budgetController.getBudget);
-app.get('/budget/:id/transactions', budgetController.getTransactions);
-app.get('/budget/:id/spend', budgetController.getSpend);
+app.post('/budget', budgetController.postBudget)
+app.get('/budget', budgetController.getBudget)
+app.post('/budget/:id/transactions', budgetController.postTransaction)
+app.get('/budget/:id/transactions', budgetController.getTransactions)
+app.get('/budget/:id/spend', budgetController.getSpend)
 
 app.listen(3000, () => {
     console.log("server runing on port 3000 yahh")
-});
+})
