aac76e9116489b35ebc38a023947aae6c9cfff1c
Efraim <tprhoahau156348@email.com>
2026-07-30 12:44:25 +0300

    חיבור ה-mysql כמעט מוכן והמונגו עם שיפצורים קלים

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -4,4 +4,24 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 
-export default {}
+let url = process.env.MYSQL_URI
+
+let pool
+try {
+    pool = mysql.createPool(url)
+    console.log("mysql pool redy (cloud)")
+} catch(err) {
+    console.log("bad mysql url or somting", err)
+}
+
+export default {
+    query: async (sql, params) => {
+        if (!pool) return [[]]
+        try {
+            return await pool.execute(sql, params)
+        } catch(e) {
+            console.log("eror in query!!!", e)
+            return [[]]
+        }
+    }
+}
