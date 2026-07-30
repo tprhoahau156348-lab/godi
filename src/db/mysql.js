9bd40ff8d2afdbe5d811a25e7261e6a5ff931a82
Efraim <tprhoahau156348@email.com>
2026-07-30 15:42:31 +0300

    עדכון קריטי לחיבור ל-sqlת השימוש עבר ל-supabase, עדיין לא עדכנתי את שם הקובץ

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,27 +1,30 @@
-import mysql from 'mysql2/promise'
+import { createClient } from '@supabase/supabase-js'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 
-let url = process.env.MYSQL_URI
+import WebSocket from 'ws'
 
-let pool
+const supabaseUrl = process.env.SUPABASE_URL || 'https://iyrbpxmntomucgcdgczj.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5cmJweG1udG9tdWNnY2RnY3pqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0MDY2NTIsImV4cCI6MjEwMDk4MjY1Mn0.Pivp42MU6KZ2pskvzN_Wr3kIq1zsXVRBdB_VTcaFEqk'
+
+let supabase = null
 try {
-    pool = mysql.createPool(url)
-    console.log("mysql pool redy (cloud)")
+    supabase = createClient(supabaseUrl, supabaseKey, {
+        auth: {
+            persistSession: false
+        },
+        global: {
+            fetch: fetch
+        },
+        realtime: {
+            transport: WebSocket
+        }
+    })
+    console.log("supabase conected succsesfully!!! (cloud)")
 } catch(err) {
-    console.log("bad mysql url or somting", err)
+    console.log("bad supabase url or somting", err)
 }
 
-export default {
-    query: async (sql, params) => {
-        if (!pool) return [[]]
-        try {
-            return await pool.execute(sql, params)
-        } catch(e) {
-            console.log("eror in query!!!", e)
-            return [[]]
-        }
-    }
-}
+export default supabase
